29f689a5ce5b3ff539b090f32679e85a1e3d30dd
Rilo <taydh.wade@gmail.com>
2022-10-26 16:15:40 -0700

    error modal no styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
+import { useState } from 'react';
 import './styles/App.css';
 import Dashboard from './components/Dashboard';
 import FrontPage from './components/FrontPage';
+import errorModal from './components/ErrorModal';
 
 function App() {
+  const[openModal, setOpenModal] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Greetings from Ekkremis.
         </p>
+        <button className = errorModalbtn onClick={() => {
+          setOpenModal(true);
+        }}>Error Log</button>
+          {openModal && <errorModal closeModal ={setOpenModal}/>}
       </header>
     </div>
   );
--- src/components/ErrorModal.tsx
+++ src/components/ErrorModal.tsx
@@ -1,5 +1,20 @@
 // Rilo
+import App from "../App";
+import React from "react";
+import "./errorModal.css";
 
-export default {
-  
+function errorModal({ closeModal }){
+  return(
+    <div className="errorModalBg">
+      <div className="modalContainer">
+        <button className="close" onClick={() =>{closeModal(false)}}>Close</button>
+        <div className="title"><h2>Error Log</h2></div>
+        <div className="body">
+          <p>image of error log goes here</p>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default errorModal;
