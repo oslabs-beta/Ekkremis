0330c0cd248e1ed0e8b9dcd397edd81e8ae8db0d
forestleigh <theforestleigh@gmail.com>
2022-10-26 17:11:10 -0700

    button and button effects are done

--- src/components/QueryBar.tsx
+++ src/components/QueryBar.tsx
@@ -7,12 +7,14 @@ function QueryBar() {
 
     return (
       <div className="query-container">
-        <button className='query-btn'>Pending</button>
-        <button className='query-btn'>Unknown</button>
-        <button className='query-btn'>Running</button>
-        <button className='query-btn'>Failed</button>
-        <button className='query-btn'>Successful</button>
-        <button className='query-btn'>Summary</button>
+        <div className='query-bar'>
+            <button className='query-btn'>Pending</button>
+            <button className='query-btn'>Unknown</button>
+            <button className='query-btn'>Running</button>
+            <button className='query-btn'>Failed</button>
+            <button className='query-btn'>Successful</button>
+            <button className='query-btn'>Summary</button>
+        </div>
       </div>
     );
   }
