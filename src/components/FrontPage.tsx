9d20a9d7211c1c8e056700083280282cc6be8fba
Cesar-Quintanilla <quintanilla.cesar@ymail.com>
2022-10-26 18:56:25 -0700

    working on Button, frontPage.css, and FrontPage

--- src/components/FrontPage.tsx
+++ src/components/FrontPage.tsx
@@ -1,17 +1,18 @@
 // Cesar
 import React from "react";
 import '../styles/frontPage.css'; // css folder
+// import logo from '../img/Ekkremis-md.png' // logo for front page not working
 import Button from './smallComps/Button'; // this should be button 
 
 function FrontPage() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
+    <div className="fp">
+        <Button />
+        <img className="fpLogo" src={'../img/Ekkremis-md.png'} alt="Ekkremis Logo"/>
+        <h1 className="fpH1">
           Ekkremis
         </h1>
-        <h3>Coming Soon ... </h3>
-      </header>
+        <h3 className="fpH3">Coming Soon ... </h3>
     </div>
   );
 }
--- src/components/smallComps/Button.tsx
+++ src/components/smallComps/Button.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 const Button = () => {
   return (
     <div>
-      <button>
-      </button>  
+      <button className='button'></button>  
     </div>
   )
 }
