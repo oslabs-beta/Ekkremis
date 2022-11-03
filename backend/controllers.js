fc6f626988c5dd7c95efb468b53816e06bc067a8
zihaoli518 <zihaoli518@gmail.com>
2022-11-03 12:03:41 -0700

    added middleware functions and modified server.js

---
+++ backend/controllers.js
@@ -0,0 +1,40 @@
+const fetch = require('node-fetch');
+
+
+const ekkremisController = {};
+
+ekkremisController.getAllPodsInfo = (req, res, next) => {
+  console.log('inside getAllPodsInfo middleware')
+  // get the prometheus end point from the request 
+  // const rootUrl = req.body.url;
+  const promql = '/api/v1/query?query=';
+  let query = '(kube_pod_status_phase)==1';
+  // const finalUrl = rootUrl + promql + query;
+  fetch('http://localhost:9090/api/v1/query?query=(kube_pod_status_phase)==1')
+    .then(data => data.json())
+    .then(data => {
+      console.log('inside 2nd then. data: ', data)
+      // format the data 
+      let resultArray = data.data.result;
+      let resultObject = {"pending": {}, "running": {}, "successful": {}, "unkown": {}, "failed": {}};
+      for (let i=0; i<resultArray.length; i++) {
+        let tempObject = {};
+        let podName = resultArray[i].metric.pod;
+        tempObject["node"] = resultArray[i].metric.namespace;
+        tempObject["status"] = resultArray[i].metric.phase;
+        tempObject["restart"] = 1;
+        tempObject["age"] = 5;
+        resultObject[resultArray[i].metric.phase.toLowerCase()][podName] = tempObject;
+      }
+      res.locals.getAllPodsInfo = resultObject;
+      return next();
+    })
+    .catch(error => {
+      console.log('caught on line 28 of controllers.js: ', error);
+    })
+}
+
+
+
+
+module.exports = ekkremisController;
--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,24 @@
 const path = require('path');
 const express = require('express');
 // import middlware controllers from controller folder
+const ekkremisController = require('./controllers');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3333;
 
-app.use(express.json());
-app.use(express.urlencoded());
+// for future post/put functionalities 
+// app.use(express.json());            
+// app.use(express.urlencoded());
 
 // set up routes 
 // set up middleware functions in controllers
   // inside the middleware functions make api calls to the url passed in 
 
+app.use('/', express.static(path.resolve(__dirname, './public/index.html')));
 
-
-app.get('/', (req, res) => {
-  
+app.get('/test', ekkremisController.getAllPodsInfo, (req, res) => { // bunch of middlewares 
+  res.status(200).json(res.locals.getAllPodsInfo)
 });
-
-
 
 
 
@@ -32,12 +32,13 @@ app.use((req, res) => {
 // global error handler
  app.use((err, req, res, next) => {
     const defaultErr = {
-      log: 'Express error handler caught unknown middleware error',
+      log: 'GE ACTIVATED: Express error handler caught unknown middleware error',
       status: 400,
       message: { err: 'An error occurred' }, 
     };
     const errorObj = Object.assign(defaultErr, err);
     console.log(errorObj.log);
+    console.log(err);
     return res.status(errorObj.status).json(errorObj.message);
   });
 
