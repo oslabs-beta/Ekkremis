36c56821d601a5be9973eddaaf57f53f98d9f692
Rilo <taydh.wade@gmail.com>
2022-11-07 10:40:39 -0800

    chart and react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
+  <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </BrowserRouter>
 );
 
